2da5183ec6fbf21e9280af07a7e381fb33fec558
loo-y <loo-y@outlook.com>
2025-09-30 12:50:29 +0800

    feat: remove tech-feed project and combine into tech-push

--- projects/tech-feed/src/worker/index.ts
+++
@@ -1,9 +0,0 @@
-import { Hono } from 'hono'
-import { huggingFaceRoutes } from './huggingface/routes'
-
-const app = new Hono<{ Bindings: Env }>()
-
-app.get('/api/', c => c.json({ name: 'Cloudflare' }))
-
-app.route('/huggingface', huggingFaceRoutes)
-export default app
